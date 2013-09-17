f089d05fa4c738afb91fc58936e737cab15159d1
Lev Khomich <lev.khomich@frumatic.com>
2013-09-17 11:40:44 +0600

    Field.set(v: T)

--- src/main/scala/com/codebranch/scala/mongodb/Field.scala
+++ src/main/scala/com/codebranch/scala/mongodb/Field.scala
@@ -1,51 +1,49 @@
 package com.codebranch.scala.mongodb
 
-class Field[T](val key : String, default : Option[T] = None)(implicit tm: Manifest[T], th: TypeHandler[Option[T]],
-                                                             fm: collection.mutable.HashMap[String, Field[_]]) extends Serializable {
+class Field[T](val key: String, default: Option[T] = None)(implicit tm: Manifest[T], th: TypeHandler[Option[T]],
+                                                           fm: collection.mutable.HashMap[String, Field[_]]) extends Serializable {
 
   thisField => fm += key -> thisField
 
-	private var _value = default
+	private var value = default
 
-	def set(v : Option[T]) {
-		_value = v
+	def set(v: Option[T]) {
+		value = v
 	}
 
-
-	def := (v : Option[T]) = set(v)
-  def := (v : T) = set(Some(v))
-
-
-	def toOption = _value
+  def set(v: T) {
+    value = Some(v)
+  }
 
 
-	def toDBObject = try {
-		th.toDBObject(_value)
-	} catch {
-		case e: UnexpectedType => {
-			Logger.error(s"Couldn't convert field `${key}` with value `${_value}" +
-			             "` to DBObject")
-			//TODO: throw specific exception
-			throw new UnexpectedType(s"Couldn't convert field `${key}` with value `${_value}" +
-			             "` to DBObject")
-		}
-	}
+	@inline def := (v: Option[T]) = set(v)
+  @inline def := (v: T) = set(v)
 
+	def toOption = value
 
-	def fromDBObject(dbObject : Object, partial: Boolean = false) : this.type = {
-		_value = th.fromDBObject(dbObject, partial)
+	def toDBObject =
+    try {
+      th.toDBObject(value)
+    } catch {
+      case e: UnexpectedType =>
+        Logger.error(s"Couldn't convert field `${key}` with value `${value}` to DBObject")
+        throw e
+    }
+
+	def fromDBObject(dbObject: Object, partial: Boolean = false): this.type = {
+		value = th.fromDBObject(dbObject, partial)
 		this
 	}
 
 
   override def equals(obj: Any): Boolean = obj match {
-    case that: Field[T] => that._value == this._value
+    case that: Field[T] => that.value == this.value
     case _ => false
   }
 
-  override def hashCode(): Int = _value.hashCode()
+  override def hashCode(): Int = value.hashCode()
 
-  override def toString = _value.toString
+  override def toString = value.toString
 }
 
 
